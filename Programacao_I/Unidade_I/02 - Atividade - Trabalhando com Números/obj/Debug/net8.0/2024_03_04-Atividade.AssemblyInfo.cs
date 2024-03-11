8b7662095f0a403773d5328e17e5a0686ae134a9
Arthur Moro <237413@unoesc.lan>
2024-03-11 19:13:14 -0300

    2024_03_11-1

--- Programacao_I/Unidade_I/02 - Atividade - Trabalhando com Números/obj/Debug/net8.0/2024_03_04-Atividade.AssemblyInfo.cs
+++ Programacao_I/Unidade_I/02 - Atividade - Trabalhando com Números/obj/Debug/net8.0/2024_03_04-Atividade.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("01 - Atividade Variáveis")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("2024_03_04-Atividade")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6c2ff2b4a486c19ec20be621b769799b8a5e838")]
-[assembly: System.Reflection.AssemblyProductAttribute("01 - Atividade Variáveis")]
-[assembly: System.Reflection.AssemblyTitleAttribute("01 - Atividade Variáveis")]
+[assembly: System.Reflection.AssemblyProductAttribute("2024_03_04-Atividade")]
+[assembly: System.Reflection.AssemblyTitleAttribute("2024_03_04-Atividade")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
